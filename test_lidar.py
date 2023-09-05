e5fc3644e3dac88217b09fae42883db68c24c916
Zach Starr <zacharyhstarr@gmail.com>
2023-09-05 19:35:16 +0800

    Updated lidar test code

--- test_lidar.py
+++ test_lidar.py
@@ -1,8 +1,11 @@
 from src.aws_iot.publish import Publisher
 from src.sensors.lidar_lite_v4 import LidarLiteV4
 from src.utils import cd_to_parent_dir, wait_for_internet
+import os
 
 cd_to_parent_dir()
+os.chdir("..")
+
 wait_for_internet()
 
 publisher = Publisher()
