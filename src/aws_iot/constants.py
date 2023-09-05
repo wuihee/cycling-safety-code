dc35834c0470fefcfec72028aaa3acd402a2ea30
wuihee <wuihee@gmail.com>
2023-09-05 11:33:56 +0800

    Updated src code

--- src/aws_iot/constants.py
+++ src/aws_iot/constants.py
@@ -1,8 +1,11 @@
+import pathlib
+
+
 class AWSConstants:
     ENDPOINT = "a1cqysdrxx4ce4-ats.iot.ap-southeast-2.amazonaws.com"
     PORT = 8883
     CLIENT_ID = "tof_sensor"
     TOPIC = "TOF"
-    AWS_CERTIFICATE = "./certs/device.pem.crt"
-    AWS_PRIVATE_KEY = "./certs/private.pem.key"
-    AWS_ROOT_CA = "./certs/Amazon-root-CA-1.pem"
+    AWS_CERTIFICATE = pathlib.Path("./certs/device.pem.crt")
+    AWS_PRIVATE_KEY = pathlib.Path("./certs/private.pem.key")
+    AWS_ROOT_CA = pathlib.Path("./certs/Amazon-root-CA-1.pem")
