4de878a5b86eeccd2bc257672e437521479db2f1
Zach Starr <zacharyhstarr@gmail.com>
2023-09-05 19:20:13 +0800

    Updated lidar test code

--- test_lidar.py
+++ test_lidar.py
@@ -2,7 +2,7 @@ from src.aws_iot.publish import Publisher
 from src.sensors.lidar_lite_v4 import LidarLiteV4
 from src.utils import wait_for_internet
 
-# wait_for_internet()
+wait_for_internet()
 
 publisher = Publisher()
 sensor = LidarLiteV4()
@@ -10,3 +10,4 @@ sensor = LidarLiteV4()
 while True:
     distance = sensor.get_distance()
     publisher.publish(distance)
+    print(distance)
