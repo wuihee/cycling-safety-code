80f5c28da43ed0edc85d67d0b19abf6ad30bdf8f
wuihee <wuihee@gmail.com>
2023-09-07 13:12:10 +0800

    Updated LIDAR test

--- scripts/test_lidar.py
+++ scripts/test_lidar.py
@@ -2,8 +2,9 @@ import os
 
 from traffic_data_sensors.aws_iot.publish import Publisher
 from traffic_data_sensors.sensors.lidar_lite_v4 import LidarLiteV4
-from traffic_data_sensors.utils import wait_for_internet
+from traffic_data_sensors.utils import cd_to_parent_dir, wait_for_internet
 
+cd_to_parent_dir()
 os.chdir("..")
 
 
