8e8c4e57b8090a13ecc9529d657ef284e31ff79e
wuihee <wuihee@gmail.com>
2023-09-07 12:42:00 +0800

    Updated test_lidar

--- scripts/test_lidar.py
+++ scripts/test_lidar.py
@@ -1,10 +1,29 @@
+import subprocess
+import time
+
 from traffic_data_sensors.aws_iot.publish import Publisher
 from traffic_data_sensors.sensors.lidar_lite_v4 import LidarLiteV4
 from traffic_data_sensors.utils import wait_for_internet
 
+
+def wait_for_i2c_device(address, bus=1, timeout=60):
+    end_time = time.time() + timeout
+    while time.time() < end_time:
+        result = subprocess.check_output(["i2cdetect", "-y", str(bus)]).decode("utf-8")
+        if address in result:
+            return True
+        time.sleep(1)
+    return False
+
+
 wait_for_internet()
 
 publisher = Publisher()
+
+if not wait_for_i2c_device("62"):
+    publisher.publish("Couldn't Connect")
+    exit()
+
 sensor = LidarLiteV4()
 
 while True:
